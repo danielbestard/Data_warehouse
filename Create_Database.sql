6f58c8fff654376d92013c3ff7ddcd17d2417a70
Roger Garriga <garriga77@gmail.com>
2016-10-13 01:11:29 +0200

    Fixed names. Fixed employee constrain problems.

--- Create_Database.sql
+++ Create_Database.sql
@@ -1,29 +1,29 @@
-#Drop database ecommerce;
+Drop database ecommerce;
 create database ecommerce;
 
 use ecommerce;
 
-create table Shippers(
+create table shippers(
 	ShipperID int(11) PRIMARY KEY,
     CompanyName nvarchar(40),
     Phone nvarchar(24)
 );
 
-create table Customers(
+create table customers(
 	CustomerID nchar(5) PRIMARY KEY,
     CompanyName nvarchar(40),
     ContactName nvarchar(30),
     ContactTitle nvarchar(30),
     Address nvarchar(60),
     City nvarchar(15),
-    Region nvarchar(10),
+    Region nvarchar(15),
     PostalCode nvarchar(10),
     Country nvarchar(15),
     Phone nvarchar(24),
     Fax nvarchar(24)
 );
 
-Create table Employees(
+Create table employees(
 	EmployeeID int(11) PRIMARY KEY,
     LastName varchar(20),
     FirstName varchar(10),
@@ -41,11 +41,11 @@ Create table Employees(
     Photo nvarchar(40),
     Notes text,
     ReportsTo int,
-FOREIGN KEY (ReportsTo) REFERENCES Employees(EmployeeId)
+FOREIGN KEY (ReportsTo) REFERENCES employees(EmployeeId)
 );
 
 
-create table Orders(
+create table orders(
 	OrderID int(11) PRIMARY KEY,
     CustomerID nchar(5),
     EmployeeID int(11),
@@ -60,19 +60,19 @@ create table Orders(
     ShipRegion nvarchar(15),
     ShipPostalCode nvarchar(10),
     ShipCountry nvarchar(15),
-foreign key (EmployeeID) references Employees(EmployeeID),
-foreign key (ShipVia) References Shippers(ShipperID),
-foreign key (CustomerID) references Customers(CustomerID)
+foreign key (EmployeeID) references employees(EmployeeID),
+foreign key (ShipVia) References shippers(ShipperID),
+foreign key (CustomerID) references customers(CustomerID)
 );
 
-Create table Categories(
+Create table categories(
 	CategoryID int(11) PRIMARY KEY,
     CategoryName nvarchar(15),
     Description text,
     Picture nvarchar(40)
 );
 
-create table Suppliers(
+create table suppliers(
 	SupplierID int(11) PRIMARY KEY,
     CompanyName nvarchar(40),
     ContactName nvarchar(30),
@@ -87,7 +87,7 @@ create table Suppliers(
     HomePage text
 );
 
-create table Products(
+create table products(
 	ProductID int(11) PRIMARY KEY,
     ProductName nvarchar(40),
     SupplierID int(11),
@@ -98,19 +98,19 @@ create table Products(
     UnitsOnOrder smallint,
     RecorderLevel smallint,
     Discontinued tinyint,
-foreign key (SupplierID) references Suppliers(SupplierID),
-foreign key (CategoryID) references Categories(CategoryID)
+foreign key (SupplierID) references suppliers(SupplierID),
+foreign key (CategoryID) references categories(CategoryID)
 );
 
-create table `Order Details`(
+create table `order_details`(
 	odID int(10) PRIMARY KEY,
     OrderID int(11),
     ProductID int(11),
     UnitPrice float,
     Quantity smallint,
     Discount real,
-foreign key (OrderID) references Orders(OrderID),
-foreign key (ProductID) references Products(ProductID)
+foreign key (OrderID) references orders(OrderID),
+foreign key (ProductID) references products(ProductID)
 );
     
 
